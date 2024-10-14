2699d6a5f1f0f4f2eeef84c0cb15c932308b3059
sakibpro58 <83116208+sakibpro58@users.noreply.github.com>
2024-10-15 05:18:09 +0600

    server.py

--- server.py
+++ server.py
@@ -2,45 +2,47 @@
 
 from libs.mx import getrecords
 from libs.email import checkemail, findcatchall
-from flask_api import FlaskAPI
+from flask import Flask, request, jsonify
 import validators
-from flask import request
 
+app = Flask(__name__)
 
+# Function to verify the email
 def verifyemail(email):
     mx = getrecords(email)
     if mx != 0:
         fake = findcatchall(email, mx)
-        if fake > 0:
-            fake = 'Yes'
-        else:
-            fake = 'No'
+        fake = 'Yes' if fake > 0 else 'No'
         results = checkemail(email, mx)
+        
         if results[0] == 666:
             return {'email': email, 'error': results[1], 'mx': mx, 'status': 'Error'}
-        if results[0] == 250:
-            status = 'Good'
-        else:
-            status = 'Bad'
+        status = 'Good' if results[0] == 250 else 'Bad'
 
-        data = {'email': email, 'mx': mx, 'code': results[0], 'message': results[1], 'status': status,
-                'catch_all': fake}
+        data = {
+            'email': email,
+            'mx': mx,
+            'code': results[0],
+            'message': results[1],
+            'status': status,
+            'catch_all': fake
+        }
         return data
     else:
         return {'error': 'Error checking email address'}
 
-
-app = FlaskAPI(__name__)
-
-
+# API route to verify email
 @app.route('/api/v1/verify/', methods=['GET'])
 def search():
-    addr = request.args.get('q')
+    addr = request.args.get('email')
+    
+    # Validate the email format
     if not validators.email(addr):
-        return {'Error': 'Invalid email address'}
+        return jsonify({'Error': 'Invalid email address'}), 400
+    
+    # Get verification result
     data = verifyemail(addr)
-    return data
-
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
